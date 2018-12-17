6aefc326dd8be877a73c1b5b4c45e794595dab58
Aldambs <36906787+Aldambs@users.noreply.github.com>
2018-12-17 19:02:49 -0200

    Add files via upload

--- Quest_11.java
+++ Quest_11.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pg_124;
 
-/**
- *
- * @author Alda Matos
- */
+import java.util.Scanner;
+
 public class Quest_11 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner ler = new Scanner(System.in);
+        int t = 0;
+        String cod_trans;
+        double valor, val_tot_vista = 0, val_tot_prazo = 0;
+        
+        for (int i = 1; i < 5; i++) {
+            System.out.println("Informe o código: ");
+            cod_trans = ler.next();
+            System.out.println("Informe o valor");
+            valor = ler.nextDouble();
+            
+            if(cod_trans.equalsIgnoreCase("V")){
+                val_tot_vista = val_tot_vista + valor;
+            }else{
+                if (cod_trans.equalsIgnoreCase("P")) {
+                    val_tot_prazo = val_tot_vista + valor;
+                }else{
+                    System.out.println("Código da transação inválido");
+                }
+            }
+        }
+        System.out.println("Valor total das compras à vista: " +val_tot_vista);
+        System.out.println("Valor total das compras à prazo: " +val_tot_prazo);
+        System.out.println("Valor total das compras efetuadas: " +val_tot_vista + val_tot_prazo);
+        System.out.println("Valor da primeira prestação das compras a prazo juntas: " +val_tot_prazo / 3);
     }
     
 }
