072a0e80b843305f7f73cd66b0fde0bc2915194e
Aldambs <36906787+Aldambs@users.noreply.github.com>
2018-12-19 00:28:09 -0200

    Add files via upload

---
+++ Quest_14.java
@@ -0,0 +1,59 @@
+package pg_124;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Quest_14 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        String cor_olhos, cor_cabelos;
+        int idade, qtd = 0, qtdID = 0, qtdOlhoAzul = 0, qtdCorCabeloR = 0;
+        double altura, peso, med_id, soma = 0, por = 0;
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+        
+        System.out.println("Informe a quantidade de pessoas a serem analisadas: ");
+        int qtdPessoas = ler.nextInt();
+        
+        for (int i = 1; i <= qtdPessoas; i++) {
+            System.out.println( i+ "º PESSOA ");
+            System.out.println("Informe a cor do cabelo: \n"
+                    + "L(louro) "
+                    + "P(preto) "
+                    + "C(castanho) "
+                    + "R(ruivo):");
+            cor_cabelos = ler.next();
+            System.out.println("Informe a cor do olhos: "
+                    + "A(azul) "
+                    + "P(preto) "
+                    + "C(castanho) "
+                    + "V(verde):");
+            cor_olhos = ler.next();
+            System.out.println("Informe a altura: ");
+            altura = ler.nextDouble();
+            System.out.println("Informe o peso: ");
+            peso = ler.nextDouble();
+            System.out.println("Informe a idade: ");
+            idade = ler.nextInt();
+            
+            if (idade > 50 && peso < 60) {
+                qtdID++;
+            }
+            if (altura < 1.50) {
+                qtd++;
+            }
+            if (cor_olhos.equalsIgnoreCase("a") ) {
+                 qtdOlhoAzul++;
+            }
+            if (cor_cabelos.equalsIgnoreCase("R") && !"a".equals(cor_olhos)) {
+                qtdCorCabeloR++;
+            }
+        }
+        System.out.println("\n");
+        System.out.println("Quantidade de pessoas com idade superior a 50 anos e peso inferior a 60 quilos: " + qtdID);
+        System.out.println("Media das idades das pessoas com altura inferior a 1,50: " + qtd);
+        System.out.println("A porcentagem de pessoas com olhos azuis: " + qtdOlhoAzul / qtdPessoas);
+        System.out.println("A quantidade de pessoas ruivas que não possuem olhos azuis: " + qtdCorCabeloR);
+    }
+    
+}
