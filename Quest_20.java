0b079290d13140f1c084122b72ed202d3b3d7429
Aldambs <36906787+Aldambs@users.noreply.github.com>
2018-12-27 20:42:46 -0200

    Add files via upload

---
+++ Quest_20.java
@@ -0,0 +1,12 @@
+package pg_124;
+
+public class Quest_20 {
+    /**
+     Faça um programa que receba dez números inteiros e mostre a quantidade de 
+     números primos dentre os números que foram digitados.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
