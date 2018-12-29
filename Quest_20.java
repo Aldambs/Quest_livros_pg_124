a755b302659de2dcd647fe64ea3038272300fec7
Aldambs <36906787+Aldambs@users.noreply.github.com>
2018-12-28 22:13:31 -0200

    Add files via upload

--- Quest_20.java
+++ Quest_20.java
@@ -5,12 +5,26 @@ import java.util.Scanner;
 public class Quest_20 {
 
     /**
-     * Faça um programa que receba dez números inteiros e mostre a quantidade de
-     * números primos dentre os números que foram digitados.
+      Faça um programa que receba dez números inteiros e mostre a quantidade de
+      números primos dentre os números que foram digitados.
      */
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int nun, primo, qtd, cont;
-        
+        int num, somaPrimo = 0, div;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Informe os números desejados: ");
+            num = ler.nextInt();
+            div = 0;
+            for (int j = 1; j <= num; j++) {
+                if(num % j == 0){
+                    div = div + 1;       
+                }
+            }
+            if((div < 2) || (div == 2)){
+                somaPrimo = somaPrimo + 1;
+            }
+        }
+        System.out.println("A qtd de primos digitados é: " +somaPrimo);
     }
 }
