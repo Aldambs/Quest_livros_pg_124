df09550e1cdcf09504ecfaad8b52789eabc8cdef
Aldambs <36906787+Aldambs@users.noreply.github.com>
2018-12-28 23:50:52 -0200

    Add files via upload

--- Quest_21.java
+++ Quest_21.java
@@ -1,16 +1,24 @@
 package pg_124;
 
-/**
- *
- * @author Alda Matos
- */
+import java.util.Scanner;
+
 public class Quest_21 {
 
     /**
-     * @param args the command line arguments
+     Escreva um programa para calcular N! (fatorial de N), sendo que o valor 
+     inteiro de N é fornecido pelo usuário. 
+     Sabe-se que: N! = 1 * 2 * 3 * ... * (N-1) * N; 0! = 1, por definição
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner ler = new Scanner(System.in);
+        int valor, n;
+        System.out.println("Informe o valor: ");
+        n = ler.nextInt();
+        valor = n;
+        for (int i = 1; i < n; i++) {
+            valor *= i;
+        }
+        System.out.println(" " +valor);
     }
     
 }
